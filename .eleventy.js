f227bcb9a75e141a0d9e589bc568446bd85f0144
Mariano Z. <marianoz@posteo.net>
2024-07-19 16:11:20 -0300

    chore: add google verfication

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,45 @@
-const { DateTime } = require("luxon");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const markdownIt = require("markdown-it");
-const markdownItCheckbox = require("markdown-it-checkbox");
+const { DateTime } = require('luxon')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const markdownIt = require('markdown-it')
+const markdownItCheckbox = require('markdown-it-checkbox')
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight)
 
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy('./src/google4e424f6f91e7baa4.html')
+  eleventyConfig.addPassthroughCopy('./src/css')
+  eleventyConfig.addPassthroughCopy('./src/assets')
 
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
 
-  eleventyConfig.addShortcode("year", () => {
-    const year = new Date().getFullYear();
-    return `${year}`;
-  });
+  eleventyConfig.addShortcode('year', () => {
+    const year = new Date().getFullYear()
+    return `${year}`
+  })
 
-  eleventyConfig.addCollection("portfolio", function (collection) {
+  eleventyConfig.addCollection('portfolio', function (collection) {
     return collection
-      .getFilteredByGlob("src/portfolio/*.md")
+      .getFilteredByGlob('src/portfolio/*.md')
       .filter((x) => !x.data.hidden)
-      .sort((a, b) => a.data.position - b.data.position);
-  });
+      .sort((a, b) => a.data.position - b.data.position)
+  })
 
   let options = {
     html: true,
-  };
+  }
 
-  let md = markdownIt(options).use(markdownItCheckbox);
-  md.render("[ ] unchecked");
-  md.render("[x] checked");
+  let md = markdownIt(options).use(markdownItCheckbox)
+  md.render('[ ] unchecked')
+  md.render('[x] checked')
 
-  eleventyConfig.setLibrary("md", md);
+  eleventyConfig.setLibrary('md', md)
 
   return {
     dir: {
-      input: "src",
-      output: "./_site",
+      input: 'src',
+      output: './_site',
     },
-  };
-};
+  }
+}
