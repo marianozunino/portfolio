7554bbc95485fdcee4b0e17f2e49c70baa0b3415
Mariano Z. <marianoz@posteo.net>
2024-11-19 13:43:28 -0300

    chore: improve ceo

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,20 @@ const { DateTime } = require('luxon')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const markdownIt = require('markdown-it')
 const markdownItCheckbox = require('markdown-it-checkbox')
+const inspect = require('util').inspect
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData('css', require('./_data/css.json'))
+
+  eleventyConfig.addFilter(
+    'debug',
+    (content) => `<pre>${inspect(content)}</pre>`
+  )
+
   eleventyConfig.addPlugin(syntaxHighlight)
 
-  eleventyConfig.addPassthroughCopy('./src/css')
   eleventyConfig.addPassthroughCopy('./src/assets')
+
   eleventyConfig.addPassthroughCopy({ './google/*': '/' })
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
@@ -35,10 +43,17 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setLibrary('md', md)
 
+  eleventyConfig.addDataExtension('json', (contents) => JSON.parse(contents))
+
+  eleventyConfig.on('eleventy.before', () => {
+    console.log('Global Data:', require('./_data/css.json'))
+  })
+
   return {
     dir: {
       input: 'src',
       output: './_site',
+      data: './_data', // Explicitly specify data directory
     },
   }
 }
